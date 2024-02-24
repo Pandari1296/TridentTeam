fe98b247b3ea3bed074fdb37fa87f874b77d1e07
Kranthi <lakshmi@uitts.com>
2024-02-24 19:07:05 +0530

    Client Entity Changes Implemented.

---
+++ BaseApplication/Entity/TridentClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseApplication.Entity;
+
+public partial class TridentClient
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int CoordinatorId { get; set; }
+
+    public bool? Status { get; set; }
+
+    public virtual Coordinator Coordinator { get; set; } = null!;
+}
